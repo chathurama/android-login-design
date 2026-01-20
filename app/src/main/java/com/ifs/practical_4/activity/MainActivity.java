491e3b10e4ccedf6bf6a0a85dd08f0ccb7d2a9b8
chathurama <153283861+chathurama@users.noreply.github.com>
2026-01-20 18:08:26 +0530

    sign in and sign up ongoing

---
+++ app/src/main/java/com/ifs/practical_4/activity/MainActivity.java
@@ -0,0 +1,63 @@
+package com.ifs.practical_4.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.ifs.practical_4.R;
+import com.ifs.practical_4.adapter.ViewPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private ViewPagerAdapter viewPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        tabLayout=findViewById(R.id.tabLayout);
+        viewPager2 = findViewById(R.id.view_pager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Sign In"));
+        tabLayout.addTab(tabLayout.newTab().setText("Sign Up"));
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        viewPagerAdapter = new ViewPagerAdapter(fragmentManager, getLifecycle());
+        viewPager2.setAdapter(viewPagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+    }
+}
